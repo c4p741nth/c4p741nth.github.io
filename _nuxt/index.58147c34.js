6e7dcd4824c5727ce5108372bf20e4e1fb2fd1af
C4P741N <prompiriya.cpt@gmail.com>
2023-11-09 17:09:00 +0700

    updated

---
+++ _nuxt/index.58147c34.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n,a as o}from"./entry.f9ee4e2b.js";const t={},s=o("br",null,null,-1),r=[s];function _(a,l){return n(),c("div",null,r)}const i=e(t,[["render",_]]);export{i as default};
